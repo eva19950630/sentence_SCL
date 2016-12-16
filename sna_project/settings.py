4b6e909a1de17af7a7263c5ae43ff183be68fbc1
LuYiFang <technology99x@gmail.com>
2016-12-16 22:27:45 +0800

    @..@

--- sna_project/settings.py
+++ sna_project/settings.py
@@ -49,7 +49,6 @@ INSTALLED_APPS = [
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
-    'allauth.socialaccount.providers.facebook',
     'allauth.socialaccount.providers.google',
 ]
 #google+
