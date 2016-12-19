151571044e1e3cb3bb817b13c33106f75d296e03
SyuanShih <fun910347@gmail.com>
2016-12-19 04:09:42 +0000

    restore forms.py

--- sentence/forms.py
+++ sentence/forms.py
@@ -5,5 +5,5 @@ class AddUser(forms.Form):
     
     class Meta:
         model = User
-        fields = ('username', 'email', 'password',)
+        fields = ('uid', 'username', 'email', 'password',)
         
