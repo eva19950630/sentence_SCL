f24b711f4641c0edd20fecfffbb7297cd6a8a23e
SyuanShih <fun910347@gmail.com>
2017-01-09 06:18:07 +0000

    login not working

--- sna_project/settings.py
+++ sna_project/settings.py
@@ -137,7 +137,11 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
+# BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, "static"),
+# )
 STATIC_URL = '/static/'
 
 #google+
