383a951ff77c4e5e0c569876abb529fe02ff5ee8
eva19950630 <fun910347@gmail.com>
2017-07-22 19:00:57 +0800

    deploy error: modify setting.py

--- sna_project/settings.py
+++ sna_project/settings.py
@@ -144,6 +144,9 @@ USE_TZ = True
 # STATICFILES_DIRS = (
 #     os.path.join(BASE_DIR, "static"),
 # )
+# STATIC_URL = '/static/'
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 STATIC_URL = '/static/'
 
 #google+
